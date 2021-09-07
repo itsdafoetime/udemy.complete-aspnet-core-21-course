843bba2bf0a980b9cca5e1f29f2248389f72c923
Cory Dafoe <cdafoe@outlook.com>
2021-09-06 20:09:47 -0400

    sec3 inital commit

---
+++ BookListRazor/Models/Book.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookListRazor.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string Author { get; set; }
+    }
+}
